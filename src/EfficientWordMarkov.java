f624341cf60fd1e92aefb3ab42918ca8a84d3ac4
Victor Chu <vic4@duke.edu>
2018-10-17 16:45:09 -0400

    checker

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class EfficientWordMarkov extends BaseWordMarkov {
 	
-	-private Map<WordGram,ArrayList<String>> myMap;
+	private Map<WordGram,ArrayList<String>> myMap;
 	
 	public EfficientWordMarkov(int order)
 	{
@@ -19,49 +19,33 @@ public class EfficientWordMarkov extends BaseWordMarkov {
 		this(3);
 	}
 	
-	private String[] stringToArray(String text)
-	{
-		String[] arrayString = new String[text.length()];
-		for(int i = 0; i < text.length();i++)
-		{
-			arrayString[i] = text.substring(i, i + 1);
-		}
-		
-		return arrayString;
-	}
 	
 	
 	@Override
 	public void setTraining(String text)
 	{
-		String trainText = text;
-		String[] textArray = stringToArray(trainText);
+		myWords= text.split("\\s+");//why do we have to do this????
 		
 		
-		for(int i = 0; i < textArray.length-getOrder()+1;i++)//loop through text
+		for(int i = 0; i < myWords.length-getOrder()+1;i++)//loop through text
 		//make sure to subtract order so we don't get out of bounds exception
 		{
-			WordGram key = new WordGram(textArray, i, getOrder());
+			WordGram key = new WordGram(myWords, i, getOrder());
 			
-			int track = textArray.length-getOrder();
-			String lastCh;
-			
+			int track = myWords.length-getOrder();
 			//this if else statement here will identify the correct 
-			if(i == track)
-				lastCh = PSEUDO_EOS;
-			else
-				lastCh = trainText.substring(i+getOrder(),i+getOrder()+1);
-			
 			if(!myMap.containsKey(key))
 			{
 				myMap.put(key, new ArrayList<String>());
 			}
 			
 			if(i == track)
-				myMap.get(key).add(lastCh);
-			else
-				myMap.get(key).add(lastCh);
-			
+			{
+				myMap.get(key).add(PSEUDO_EOS);
+			}
+			else{
+				myMap.get(key).add(myWords[i+getOrder()]);
+			}
 		}
 		
 	}
